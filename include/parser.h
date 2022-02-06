206999d9d8fbb30853ee5b0195f7e9417ed77f2f
sherknob <niklasromanowski7@gmail.com>
2022-02-06 16:43:14 +0100

    adding InsertIntoDictionary function to, well... insert into Dictionary :)

--- include/parser.h
+++ include/parser.h
@@ -4,11 +4,14 @@
 #include <stdint.h>
 #include <inttypes.h>
 
+#define DEBUG 0
+
+typedef enum Type{Key, Modifier, Keyword, Custom}Type;
 
 /*
-*  Struct Key
+*  Struct Entry
 *
-*  Holds information about a Key
+*  Holds information about an Entry
 *
 *  @var keyword_length = length of the keyword the @struct Keys
 *    will be searched upon
@@ -18,40 +21,26 @@
 *    - Second 8 bits (read from right to left) = Key value
 *
 */
-typedef struct Key{
+typedef struct Entry{
+  Type type;
   u_int8_t keyword_length;
   char * keyword;
   u_int16_t value;
-}Key;
+}Entry;
 
 /*
- *  Struct Keywords
+ *  Struct Dictionary
  *
- *  Holds Keywords like DELAY, STRING, etc...
+ *  Holds a number of @struct Entry
  *
- *  @var keyword_length = length of @var keyword
- *  @var entries = Holds keyword as STRING so I can do strcmp
- *
- */
-typedef struct Keywords{
-  u_int8_t quantity;
-  Key * entries;
-}Keywords;
-
-
-/*
- *  Struct Keys
- *
- *  Holds a number of @struct Key
- *
- *  @var quantity = Holds the number of entries in @var keys
- *  @var entries = Holds pointers to @struct Key
+ *  @var quantity = Holds the number of entries in @var entries
+ *  @var entries = Holds pointers to @struct Entry
  *
  */
-typedef struct Keys{
+typedef struct Dictionary{
   u_int16_t quantity;
-  Key * entries;
-}Keys;
+  Entry * entries;
+}Dictionary;
 
 
 /*
@@ -119,3 +108,62 @@ static void printSplitLine(SplitLine * sl){
     printf("\n%s\n", "-----------------------------");
   }
 }
+
+
+static void insertIntoDictionary(Dictionary * _dictionary,
+                                  u_int8_t keyword_length,
+                                  char * keyword,
+                                  char * value,
+                                  Type type
+                                )
+{
+  // Make space for another entry
+  if(DEBUG) printf("%s\n", "Allocating memory for new entry");
+  _dictionary->entries = realloc(_dictionary->entries, sizeof(Entry) * (_dictionary->quantity+1));
+  if(DEBUG) printf("\t%s: %d\n", "Keyword_length", keyword_length);
+  _dictionary->entries[_dictionary->quantity].keyword = malloc(keyword_length);
+  _dictionary->entries[_dictionary->quantity].keyword_length = keyword_length;
+  // strcpy(_dictionary->entries[_dictionary->quantity].keyword, keyword);
+  // _dictionary->entries[_dictionary->quantity].value = (u_int16_t)*value;
+  // _dictionary->entries[_dictionary->quantity].type = type;
+  if(DEBUG) printf("\t%s: %p\n", "Adress of Entry", &_dictionary->entries[_dictionary->quantity]);
+  if(DEBUG) printf("\t%s: %p\n", "Adress of Entry.keyword", _dictionary->entries[_dictionary->quantity].keyword);
+  if(DEBUG) printf("\t%s: %d\n", "Entry.keyword_length", _dictionary->entries[_dictionary->quantity].keyword_length);
+  _dictionary->quantity += 1;
+  if(DEBUG) printf("\t%s: %d\n", "Dictionary->quantity", _dictionary->quantity);
+  if(DEBUG) printf("%s\n\n", "Done");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
