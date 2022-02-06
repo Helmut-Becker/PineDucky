bbaca1a25387728e8aa5c924ceb9555c4bbd6d96
sherknob <niklasromanowski7@gmail.com>
2022-02-06 17:02:48 +0100

    adding fileOpen and Close functions

--- include/parser.h
+++ include/parser.h
@@ -4,7 +4,7 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-#define DEBUG 0
+#define DEBUG 1
 
 typedef enum Type{Key, Modifier, Keyword, Custom}Type;
 
@@ -123,19 +123,50 @@ static void insertIntoDictionary(Dictionary * _dictionary,
   if(DEBUG) printf("\t%s: %d\n", "Keyword_length", keyword_length);
   _dictionary->entries[_dictionary->quantity].keyword = malloc(keyword_length);
   _dictionary->entries[_dictionary->quantity].keyword_length = keyword_length;
-  // strcpy(_dictionary->entries[_dictionary->quantity].keyword, keyword);
-  // _dictionary->entries[_dictionary->quantity].value = (u_int16_t)*value;
-  // _dictionary->entries[_dictionary->quantity].type = type;
+  strcpy(_dictionary->entries[_dictionary->quantity].keyword, keyword);
+  _dictionary->entries[_dictionary->quantity].value = (u_int16_t) strtol(value, NULL, 16);
+  _dictionary->entries[_dictionary->quantity].type = type;
   if(DEBUG) printf("\t%s: %p\n", "Adress of Entry", &_dictionary->entries[_dictionary->quantity]);
   if(DEBUG) printf("\t%s: %p\n", "Adress of Entry.keyword", _dictionary->entries[_dictionary->quantity].keyword);
   if(DEBUG) printf("\t%s: %d\n", "Entry.keyword_length", _dictionary->entries[_dictionary->quantity].keyword_length);
+  if(DEBUG) printf("\t%s: %d\n", "Entry.type", _dictionary->entries[_dictionary->quantity].type);
+  if(DEBUG) printf("\t%s: %d\n", "Entry.keyword_length", _dictionary->entries[_dictionary->quantity].keyword_length);
+  if(DEBUG) printf("\t%s: %s\n", "Keyword", _dictionary->entries[_dictionary->quantity].keyword);
+  if(DEBUG) printf("\t%s: %x\n", "Value", _dictionary->entries[_dictionary->quantity].value);
   _dictionary->quantity += 1;
   if(DEBUG) printf("\t%s: %d\n", "Dictionary->quantity", _dictionary->quantity);
   if(DEBUG) printf("%s\n\n", "Done");
 }
 
 
+/*
+ *  Function openFile
+ *
+ *  Opens file and @return filepointer
+ *
+ */
+static FILE * openFile(char * path){
+  FILE * fp;
+  fp = fopen(path, "r");
+  if (fp == NULL){
+    printf("%s\n", "File not found, exiting...");
+    exit(EXIT_FAILURE);
+  }
+  return fp;
+}
 
+/*
+ *  Function closeFile
+ *
+ *  Closes file @param fp and frees @param line
+ *
+ */
+static void closeFile(FILE * fp, char * line){
+  fclose(fp);
+  if (line)
+      free(line);
+  exit(EXIT_SUCCESS);
+}
 
 
 
