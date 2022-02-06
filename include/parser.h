86cef946a118b1123c78fcaa57bb1b04f435321f
sherknob <niklasromanowski7@gmail.com>
2022-02-06 12:38:07 +0100

    adding printSplitLine method to print all contents of SplitLine struct onto console

--- include/parser.h
+++ include/parser.h
@@ -27,10 +27,45 @@ typedef struct Sequence{
  *    smaller pieces, to evaluate them all one by one.
  *
  *  @var quantity = Holds number of entries in slices
+ *  @var length = Holds pointers to length of slices
  *  @var slices = Holds slices of strings
  *
  */
 typedef struct SplitLine{
   u_int8_t quantity;
+  u_int8_t * length;
   char ** slices;
 }SplitLine;
+
+
+/*
+ *  Struct Script
+ *
+ *  Struct to hold Bytesequences of 8 bytes each
+ *
+ *  @var quantity = Holds number of entries in sequences
+ *  @var sequences = Holds Bytesequences
+ */
+typedef struct Script{
+  u_int8_t quantity;
+  char ** sequences;
+}Script;
+
+
+/*
+ *  Function printSplitLine
+ *
+ *  Prints all contents of split line onto STDOUT
+ *
+ */
+static void printSplitLine(SplitLine * sl){
+  printf("Quantity: %d\n", sl->quantity);
+  for (size_t i = 0; i < sl->quantity; i++){
+    printf("%s: %d\n", "Slice", i);
+    printf("\t%s: %d\n\t", "length", sl->length[i]);
+    for (size_t j = 0; j < sl->length[i]; j++) {
+      printf("%c", sl->slices[i][j]);
+    }
+    printf("\n%s\n", "-----------------------------");
+  }
+}
