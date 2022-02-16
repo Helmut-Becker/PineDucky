a6e9e2f058bd9d669a26acaca87d8eb61c9a517f
sherknob <niklasromanowski7@gmail.com>
2022-02-16 18:08:19 +0100

    adding basic usage

--- include/pineDucky.h
+++ include/pineDucky.h
@@ -1,18 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void help(){
-  printf("%s", "Usage:");
-  printf("%s", " pinephone-tools [args] < [script]\n");
-  printf("%s", "[args]:\n\thelp");
-  printf("%s", "\n");
+  printf("%s\n", "Usage:");
+  printf("\t%s\n\n", "./pineDucky [script-file] [outfile]");
+  printf("\t%s\n", "[script-file]: DuckyScript Language is read from.");
+  printf("\t%s\n\n", "[outfile]: Sequences are written into and will be executed");
+  printf("\t%s\n", "Note: To turn DEBUG on, change DEBUG to 1 in parser.h and recompile");
 }
 
-
 void evaluateArgs(char **argv){
-
-  if (argv[1] == NULL) return;
-  else if (strcmp(argv[1], "help") == 0) help();
-
+  if (argv[1] == NULL || argv[2] == NULL) exit(-1);
+  else if (strcmp(argv[1], "help") == 0){
+    help();
+    exit(-1);
+  }
 }
 void parseScript(char ** argv);
--- pineDucky.c
+++ pineDucky.c
@@ -2,10 +2,7 @@
 #include "parser.h"
 
 int main(int argc, char **argv){
-
   evaluateArgs(argv);
-
   parseScript(argv);
-
   return 0;
 }
